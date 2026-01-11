1e53a2391394ee8e5583b15737258f15d7ec3570
treb <bjvc1104@gmail.com>
2026-01-11 16:35:46 -0500

    Correcciones

--- 2025_1/PC1/problem2.cpp
+++ 2025_1/PC1/problem2.cpp
@@ -3,20 +3,23 @@ int main()
 {
 	int n = 8;
 	int array[n];
+
 	for (int i = 0; i < n; i++){
 		int num = 0;
 		std::cin >> num;
 		array[i] = num;
 	}
 
-	int numAparece1Vez = 0;
+	int numAparece1Vez = -1;
 	int countNum = 0;
 
 	for (int i = 0; i < n; i++){
+		countNum = 0;
+
 		for (int j = 0; j < n; j++){
 			if (array[i] == array[j]){
 				countNum++;
-				if (countNum == 2){
+				if (countNum > 1){
 					break;
 				}
 			}
@@ -25,13 +28,8 @@ int main()
 			numAparece1Vez = array[i];
 			break;
 		}
-		countNum = 0;
+		
 	}
-	if (countNum == 0){
-		std::cout << -1 << "\n";
-	}
-	else{
-		std::cout << numAparece1Vez << "\n";
-	}
-	return 0;
+
+	std::cout << numAparece1Vez << "\n1;"
 }
