496aa253c7dfdc8657a4c0db8d3a3a5486f80ccf
treb <bjvc1104@gmail.com>
2026-03-17 17:10:45 -0500

    Add problem1.jpg and solve problem1

--- 2025_1/PC1/problem1.cpp
+++ 2025_1/PC1/problem1.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <cmath>
+
+double b(int n);
+double fract(int k, int n);
+
 int main(){
-    std::cout << ":)";
+    std::cout << "Ingrese un entero n: ";
+    int n {};
+    std::cin >> n;
+
+    double x { 1 + 1.0 / fract(1,  n)};
+    std::cout << "El valor de x es: " << x << '\n';
     return 0;
+}
+
+
+double b(int n) {
+    double sign = ( n % 2 == 0 ) ? 1.0 : -1.0;
+    return sign * std::ceil(n/ 2.0);
+}
+
+double fract(int k, int n){
+    if (k == n) {
+        return n;
+    }
+    return k + b(k)/ fract(k + 1, n);
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Fundamentos de Programacion
 Hecho con fines academicos.
 
-Solucionario de los problemas propuestos en las pd's, pc's, examenes parciales y finales 
+Solucionario de los problemas propuestos en las pd's, pc's, examenes parciales y finales
